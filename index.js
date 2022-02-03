fef26b7369a4059fecd458246526adaa2856f7ea
Gian Taino Carlos <giantainocarlos@gmail.com>
2022-02-03 14:07:59 -0600

    commit tree 6ebf27300e07a51f572bfac19793ab0e89d105c2

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(array) {
+    return array.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(array) {
+    return array.slice(-2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num1) {
+    return function(num2) {
+        return num1 * num2;
+    }
+}
+
+function fareDoubler(num3) {
+    return num3 * 2;
+}
+
+function fareTripler(num4) {
+    return num4 * 3;
+}
+
+function selectDifferentDrivers(array, whichArray) {
+    if (whichArray === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(array);
+    }
+    else if (whichArray === returnLastTwoDrivers) {
+        return returnLastTwoDrivers(array);
+    }
+}
